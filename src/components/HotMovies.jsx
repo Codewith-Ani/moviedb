93ae0796ee96bca9aaf1fe03fe42c1fb1c26ed4f
Animesh <animeshjha3@gmail.com>
2023-04-16 17:05:28 +0530

    Working on HOT PAGE

--- src/components/HotMovies.jsx
+++ src/components/HotMovies.jsx
@@ -3,26 +3,25 @@ import React, { useEffect, useState } from 'react';
 import '../styles/HotMovies.css';
 import Fire from '../assets/Fire.png';
 import MovieCard from './MovieCard';
+import Avatar from '../assets/movieImg.jpg';
 import axios from 'axios';
 
 const HotMovies = () => {
-	const [popularMovies, setPopularMovies] = useState();
+	const [movies, setMovies] = useState([]);
+	let url =
+		'https://api.themoviedb.org/3/movie/popular?api_key=eaf448398d979ad267711b4ff1e492e4&language=en-US&page=1';
 
-	let API_key = 'eaf448398d979ad267711b4ff1e492e4';
-	let baseUrl = 'https://api.themoviedb.org/3';
-
-	const getPopularMovies = async () => {
+	const getMovies = async () => {
 		try {
-			const { data } = await axios.get(
-				baseUrl + '/movie/popular?api_key=' + API_key + '&language=en-US&page=1'
-			);
-			setPopularMovies(data.results);
-		} catch (err) {
-			console.log(err);
+			const { data } = await axios.get(url);
+			setMovies(data.results);
+		} catch (error) {
+			console.log(error);
 		}
 	};
+
 	useEffect(() => {
-		getPopularMovies();
+		getMovies();
 	}, []);
 
 	return (
@@ -35,7 +34,18 @@ const HotMovies = () => {
 				/>
 			</span>
 			<div className='hotmovies_CardContainer'>
-				{console.log(typeof [popularMovies])}
+				{movies.length === 0
+					? '<p> Not Found </p>'
+					: movies.map((movie) => {
+							console.log(movie.original_title);
+
+							<MovieCard
+								key={movie.id}
+								movieImg={Avatar}
+								Name={movie.original_title}
+								rating={movie.vote_average}
+							/>;
+					  })}
 			</div>
 		</div>
 	);
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,13 +4,30 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import { CardActionArea } from '@mui/material';
-import Avatar from '../assets/movieImg.jpg';
 
 import '../styles/MovieCard.css';
 import StarIcon from '@mui/icons-material/Star';
+
 const MovieCard = (props) => {
-	console.log('Hello MovieCard');
-	return props;
+	return (
+		<Card className='Card'>
+			<CardActionArea className='Card_CardArea'>
+				<CardMedia
+					className='CardMedia'
+					component='img'
+					image={props.movieImg}
+					alt='green iguana'
+					style={{ width: '150px', height: '225px' }}
+				/>
+				<CardContent className='Card_CardContent'>
+					<div className='card_text'>
+						{props.rating} <StarIcon className='Star' />
+					</div>
+					<div>{props.Name}</div>
+				</CardContent>
+			</CardActionArea>
+		</Card>
+	);
 };
 
 export default MovieCard;
